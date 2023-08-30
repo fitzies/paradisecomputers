736fda385bc5ce6e30983f9af7dc4e0d25797d96
Mr Bushido <114846273+fitzies@users.noreply.github.com>
2023-08-30 22:39:43 +0800

    fixed product sizing

--- src/components/ProductView.tsx
+++ src/components/ProductView.tsx
@@ -25,13 +25,13 @@ const ProductView: React.FC<ProductViewProps> = ({
       className="border-2 px-4 py-6 w-full rounded-xl flex items-center levitate"
       href={`/products/${slug}`}
     >
-      <div className="gap-1 flex flex-col">
+      <div className="gap-1 flex flex-col w-4/5 h-3/4">
         <h2 className="text-gray-800 text-sm">R{price}</h2>
         <h1 className="font-bold text-xl">{productTitle}</h1>
         <p className="text-gray-600">{productText}</p>
       </div>
       <div className="w-4/5 aspect-square ml-auto flex justify-center items-center">
-        <img src={image} alt={productTitle} className="" />
+        <img src={image} alt={productTitle} className="w-full" />
       </div>
     </Link>
   );
